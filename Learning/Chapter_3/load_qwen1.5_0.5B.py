074a3fd1fd4d1348fb10610d22a1d29714d30879
Daiku <kokujoujihuang@gmail.com>
2026-02-24 10:22:56 +0800

    增加千问问答

--- Learning/Chapter_3/load_qwen1.5_0.5B.py
+++ Learning/Chapter_3/load_qwen1.5_0.5B.py
@@ -9,9 +9,48 @@ device = "Cuda" if torch.cuda.is_available() else "cpu"
 print("using device: {}".format(device))
 
 # 加载分词器
-tokenizer = AutoTokenizer.form_pretrained(model_id)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 # 加载模型，并将其移动到指定设备
 model = AutoModelForCausalLM.from_pretrained(model_id).to(device)
 
 print("模型和分词器加载完成")
+print("准备对话")
+
+# 准备对话
+message = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "你好，请介绍你自己。"}
+]
+
+# 使用分词器的模板格式化输入
+text = tokenizer.apply_chat_template(
+    message,
+    tokenize=False,
+    add_generation_prompt=True
+)
+
+model_inputs = tokenizer([text], return_tensors="pt").to(device)
+
+print("编码后输入的文本：")
+print(model_inputs)
+
+# 使用模型生成回答
+print("使用模型生成回答")
+# max_new_tokens 控制了模型最多能生成多少个新的Tokens
+generated_ids = model.generate(
+    model_inputs.input_ids,
+    max_new_tokens=512
+)
+
+# 将生成的Token ID截取掉输入部分
+# 这样我们只解码模型新生成的部分
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+# 解码生成的Token ID
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+print("模型的回答：")
+print(response)
